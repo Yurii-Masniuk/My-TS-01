3839c3c1c24267c213a4c2e89587c34f59f4fc99
Yurii-Masniuk <yeristar@gmail.com>
2025-09-18 19:16:12 +0300

    task-08

---
+++ src/task-8.ts
@@ -0,0 +1,24 @@
+
+
+enum Role {
+    Admin = 'Admin',
+    User = 'User',
+    Guest = 'Guest',
+};
+
+function getPermissions(role: Role): string[] {
+    switch (role) {
+        case Role.Admin:
+            return ["create", "read", "update", "delete"];
+        case Role.User:
+            return ["read", "update"];
+        case Role.Guest:
+            return ["read"];
+        default:
+            return [];
+    };
+}
+
+console.log(getPermissions(Role.Admin));
+console.log(getPermissions(Role.User));
+console.log(getPermissions(Role.Guest));

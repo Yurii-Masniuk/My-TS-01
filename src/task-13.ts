f68dc301f32a6525a27a904c78871470e1dc731f
Yurii-Masniuk <yeristar@gmail.com>
2025-09-18 21:59:23 +0300

    task-13

---
+++ src/task-13.ts
@@ -0,0 +1,15 @@
+type Action = { type: 'increment' | 'decrement' };
+
+function reducer(state: number, action: Action): number {
+    switch (action.type) {
+        case "increment":
+            return state + 1;
+        case "decrement":
+            return state - 1;
+        default:
+            return state;
+    }
+}
+
+console.log('reducer', reducer(0, { type: 'increment' }));
+console.log('reducer', reducer(5, { type: 'decrement' }));

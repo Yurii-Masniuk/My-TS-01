4b79085a04dab8d105b17af93d91004625bcd10f
Yurii-Masniuk <yeristar@gmail.com>
2025-09-18 22:48:27 +0300

    task-19

---
+++ src/task-19.ts
@@ -0,0 +1,39 @@
+import axios from "axios";
+
+interface User {
+
+    email: string,
+    id: number,
+    name: string,
+    phone: number,
+    username: string,
+    website: string,
+    company: Company,
+    address: Address,
+};
+
+interface Company {
+    bs: string,
+    catchPhrase: string,
+    name: string,
+};
+
+interface Address {
+    city: string,
+    geo: { lat: string, lng: string },
+    street: string,
+    suite: string,
+    zipcode: string,
+}
+
+const fetchUser = async (userId: number): Promise<User> => {
+    const response = await axios.get<User>(`https://jsonplaceholder.typicode.com/users/${userId}`);
+    return response.data;
+};
+
+const getUserName = async (id: number) => {
+    const user = await fetchUser(id);
+    console.log(user.name);
+};
+
+getUserName(3);

0c579ec1cfc019df5a418cea0a74f7540c79b164
Yurii-Masniuk <yeristar@gmail.com>
2025-09-18 13:17:27 +0300

    task-02

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,3 @@
 import './style.css'
 import './task-1.ts'
+import './task-2.ts'
---
+++ src/task-2.ts
@@ -0,0 +1,12 @@
+type Currency = 'USD' | 'EUR' | 'UAH';
+
+interface convertCurrencyParams {
+    amount: number,
+    currency: Currency,
+}
+
+function convertCurrency({ amount, currency }: convertCurrencyParams): void {
+    console.log(`Converting ${amount} to ${currency}`);
+}
+
+convertCurrency({ amount: 500, currency: 'EUR' });
